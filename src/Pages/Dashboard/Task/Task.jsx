4ca31e440e9e6003da283f8213f9b57ae3375f1e
Shuvajit Maitra <shuvajitmaitra@gmail.com>
2023-12-22 06:14:09 +0600

    connect with backend and task page create

---
+++ src/Pages/Dashboard/Task/Task.jsx
@@ -0,0 +1,13 @@
+
+
+const Task = () => {
+
+
+    return(
+        <div className="flex justify-center item-center gap-6">
+             <div className="shadow-md rounded-lg border h-96"></div>
+             <div className="shadow-md rounded-lg border h-96"></div>
+             <div className="shadow-md rounded-lg border h-96"></div>
+        </div>
+    )}
+export default Task;
